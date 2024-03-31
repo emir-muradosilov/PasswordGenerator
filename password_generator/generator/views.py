7430c680f0d8e66c02cac77d8fe26238655df6a4
emir-muradosilov <emir.muradosilov@yandex.ru>
2024-03-31 18:19:47 +0300

    Add logic to generate password with: special simbols and numbers

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -1,25 +1,30 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-
+import random as r
 
 def home(request):
     return render(request, 'generator/home.html', {'home':'This is my home!'})
 # Create your views here.
 
 def password(request):
-    return render(request, 'generator/password.html', {'password':generate_password()})
+
+    characters = [chr(i) for i in range(97,123)]
+    chisla = [i for i in range(0,10)]
+    specialsimvol = [chr(i) for i in range(33,48)]
+    if request.GET.get('numbers') == 'on':
+        characters += chisla
+    if request.GET.get('specialsimvol') == 'on':
+        characters += specialsimvol
+    length = int(request.GET.get('length', 8))
+    password = ''
+    for i in range(length):
+        var = r.choice(['l','a'])
+        password += str(r.choice(characters)) if var == 'l' else str(r.choice(characters)).upper()
+
+    return render(request, 'generator/password.html', {'password' : password })
 # Create your views here.
 
-def generate_password():
-    import random as r
-    alphavit = [chr(i) for i in range(97,123)]
-    chisla = [i for i in range(0,10)]
-    res = alphavit + chisla
-    rand=''
-    for i in range(10):
-        rand += str(r.choice(res))
-    return rand
 
 def test(request):
     return HttpResponse('test - page')
