e6ac6f6a275bada631938ffc5da87c4e5d61fcab
emir-muradosilov <emir.muradosilov@yandex.ru>
2024-03-31 03:24:32 +0300

    Add 2 pages. And add password generater in test mod

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request, 'generator/home.html', {'home':'This is my home!'})
 # Create your views here.
+
+def password(request):
+    return render(request, 'generator/password.html', {'password':generate_password()})
+# Create your views here.
+
+def generate_password():
+    import random as r
+    alphavit = [chr(i) for i in range(97,123)]
+    chisla = [i for i in range(0,10)]
+    res = alphavit + chisla
+    rand=''
+    for i in range(10):
+        rand += str(r.choice(res))
+    return rand
+
+def test(request):
+    return HttpResponse('test - page')
